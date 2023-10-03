da194107de5b2da60bc0f5f2bec06ec00908bb05
dileepadari <adaridileep@gmail.com>
2023-10-03 16:00:45 +0530

    oct 3

---
+++ src/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template, redirect, session, url_for, jsonify
+import sqlite3
+import requests
+import json
+
+app = Flask(__name__)
+app.secret_key = 'DelhiisDelhi'
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    headers = {'X-M2M-Origin': 'admin:admin', "Content-Type": "application/json;ty=4"}
+    link = "https://api.thingspeak.com/channels/2281910/feeds.json?results=2"
+    response = requests.get(link, headers=headers)
+    response = json.loads(response.text)
+    print(response)
+    return render_template('index.html', data = response, file = "home.html", open1="open")
+
+@app.route('/statistics', methods=['GET','POST'])
+def statistics():
+    return render_template('index.html', file = "statistics.html", open2="open")
+
+# @app.route('/statitics', methods=['GET','POST'])
+# def statistics():
+#     return render_template('statistics.html')
+
+# @app.route('/statitics', methods=['GET','POST'])
+# def statistics():
+#     return render_template('statistics.html')
+
+# @app.route('/statitics', methods=['GET','POST'])
+# def statistics():
+#     return render_template('statistics.html')
+
+# @app.route('/statitics', methods=['GET','POST'])
+# def statistics():
+#     return render_template('statistics.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
